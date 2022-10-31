003f9daff538bdcf07970548fc4ed5ad7d78204f
Arif5577 <arifalamm33@gmail.com>
2022-10-31 09:04:45 +0600

    404 page added

--- client/src/Routes/Routes.js
+++ client/src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import NotFound from "../pages/NotFound/NotFound";
 import Signin from "../pages/Signin/Signin";
 
 const RootRoutes = createBrowserRouter([
@@ -12,6 +13,7 @@ const RootRoutes = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "/login", element: <Login /> },
       { path: "/signin", element: <Signin /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
